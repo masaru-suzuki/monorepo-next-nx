c5e2e9e8860ab863fe544c7877bff650b4e9e3e5
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-06-19 13:29:39 +0900

    add: page.tsxでblog一覧表示

--- nx-monorepo/apps/app02/app/page.tsx
+++ nx-monorepo/apps/app02/app/page.tsx
@@ -1,15 +1,37 @@
 import styles from './page.module.scss';
+import Link from 'next/link';
+import { getList } from '../../../libs/client';
 
 export default async function Index() {
+  const { contents } = await getList();
+
+  const time = new Date().toLocaleDateString();
+
+  if (!contents || contents.length === 0)
+    return <div>記事データがありません</div>;
+
   /*
    * Replace the elements below with your own.
    *
    * Note: The corresponding styles are in the ./index.scss file.
    */
+
   return (
     <div className={styles.page}>
       <div className={styles.wrapper}>
         <h1>Welcome to app02!</h1>
+        <p>{time}</p>
+        <div>
+          <ul>
+            {contents.map((post) => {
+              return (
+                <li key={post.id}>
+                  <Link href={`/app02/${post.id}`}>{post.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
