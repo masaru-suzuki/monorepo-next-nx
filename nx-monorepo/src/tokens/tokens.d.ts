979e66bbceffceae8d4658ece19e54fc78bea754
masaru suzuki <masaru.suzuki.yamagata@gmail.com>
2023-06-20 09:53:47 +0900

    add: デザイントークン連携

---
+++ nx-monorepo/src/tokens/tokens.d.ts
@@ -0,0 +1,45 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 19 Jun 2023 21:15:37 GMT
+ */
+
+export default tokens;
+
+declare interface DesignToken {
+  value: any;
+  name?: string;
+  comment?: string;
+  themeable?: boolean;
+  attributes?: {
+    category?: string;
+    type?: string;
+    item?: string;
+    subitem?: string;
+    state?: string;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+declare const tokens: {
+  "theme-color": DesignToken,
+  "secondary-color": DesignToken,
+  "brandA": {
+    "theme": DesignToken,
+    "secondary": DesignToken
+  },
+  "brandB": {
+    "theme": DesignToken,
+    "secondary": DesignToken
+  },
+  "brandC": {
+    "theme": DesignToken,
+    "secondary": DesignToken
+  },
+  "tokenSetOrder": {
+    "0": DesignToken,
+    "1": DesignToken,
+    "2": DesignToken,
+    "3": DesignToken
+  }
+}
---
+++ nx-monorepo/src/tokens/tokens.js
@@ -0,0 +1,239 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 19 Jun 2023 21:15:37 GMT
+ */
+
+module.exports = {
+  "theme-color": {
+    "value": "#00ff57",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#00ff57",
+      "type": "color"
+    },
+    "name": "ThemeColor",
+    "attributes": {
+      "category": "theme-color"
+    },
+    "path": [
+      "theme-color"
+    ]
+  },
+  "secondary-color": {
+    "value": "#403980",
+    "type": "color",
+    "filePath": "output.json",
+    "isSource": true,
+    "original": {
+      "value": "#403980",
+      "type": "color"
+    },
+    "name": "SecondaryColor",
+    "attributes": {
+      "category": "secondary-color"
+    },
+    "path": [
+      "secondary-color"
+    ]
+  },
+  "brandA": {
+    "theme": {
+      "value": "#f00",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#f00",
+        "type": "color"
+      },
+      "name": "BrandATheme",
+      "attributes": {
+        "category": "brandA",
+        "type": "theme"
+      },
+      "path": [
+        "brandA",
+        "theme"
+      ]
+    },
+    "secondary": {
+      "value": "#ff9898",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#ff9898",
+        "type": "color"
+      },
+      "name": "BrandASecondary",
+      "attributes": {
+        "category": "brandA",
+        "type": "secondary"
+      },
+      "path": [
+        "brandA",
+        "secondary"
+      ]
+    }
+  },
+  "brandB": {
+    "theme": {
+      "value": "#efff00",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#efff00",
+        "type": "color"
+      },
+      "name": "BrandBTheme",
+      "attributes": {
+        "category": "brandB",
+        "type": "theme"
+      },
+      "path": [
+        "brandB",
+        "theme"
+      ]
+    },
+    "secondary": {
+      "value": "#f9ffa3",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#f9ffa3",
+        "type": "color"
+      },
+      "name": "BrandBSecondary",
+      "attributes": {
+        "category": "brandB",
+        "type": "secondary"
+      },
+      "path": [
+        "brandB",
+        "secondary"
+      ]
+    }
+  },
+  "brandC": {
+    "theme": {
+      "value": "#0012ff",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#0012ff",
+        "type": "color"
+      },
+      "name": "BrandCTheme",
+      "attributes": {
+        "category": "brandC",
+        "type": "theme"
+      },
+      "path": [
+        "brandC",
+        "theme"
+      ]
+    },
+    "secondary": {
+      "value": "#bec1ff",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#bec1ff",
+        "type": "color"
+      },
+      "name": "BrandCSecondary",
+      "attributes": {
+        "category": "brandC",
+        "type": "secondary"
+      },
+      "path": [
+        "brandC",
+        "secondary"
+      ]
+    }
+  },
+  "tokenSetOrder": {
+    "0": {
+      "value": "brandA",
+      "type": "other",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "brandA",
+        "type": "other"
+      },
+      "name": "TokenSetOrder0",
+      "attributes": {
+        "category": "tokenSetOrder",
+        "type": "0"
+      },
+      "path": [
+        "tokenSetOrder",
+        "0"
+      ]
+    },
+    "1": {
+      "value": "brandB",
+      "type": "other",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "brandB",
+        "type": "other"
+      },
+      "name": "TokenSetOrder1",
+      "attributes": {
+        "category": "tokenSetOrder",
+        "type": "1"
+      },
+      "path": [
+        "tokenSetOrder",
+        "1"
+      ]
+    },
+    "2": {
+      "value": "brandC",
+      "type": "other",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "brandC",
+        "type": "other"
+      },
+      "name": "TokenSetOrder2",
+      "attributes": {
+        "category": "tokenSetOrder",
+        "type": "2"
+      },
+      "path": [
+        "tokenSetOrder",
+        "2"
+      ]
+    },
+    "3": {
+      "value": "brandD",
+      "type": "other",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "brandD",
+        "type": "other"
+      },
+      "name": "TokenSetOrder3",
+      "attributes": {
+        "category": "tokenSetOrder",
+        "type": "3"
+      },
+      "path": [
+        "tokenSetOrder",
+        "3"
+      ]
+    }
+  }
+};
